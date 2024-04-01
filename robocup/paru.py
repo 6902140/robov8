aeab04a85d5e4b7fd4d3700f4358058913e395f0
Zhuiri Xiao <3356142450@qq.com>
2024-04-01 11:57:41 +0800

    add no desk mode

--- robocup/paru.py
+++ robocup/paru.py
@@ -52,15 +52,15 @@ class Paru(object):
 
         detected_imgs.append(result.plot())
         resized_image = cv2.resize(result.plot(), (400, 300))
-        # cv2.imshow("test",resized_image)
-        # cv2.waitKey(0)
-        # print(results)
-        # print(results[0].boxes)
+        cv2.imshow("test",resized_image)
+        cv2.waitKey(0)
+        print(results)
+        print(results[0].boxes)
         return results,detected_imgs
 
 
 # just for testing purposes
 if __name__ == '__main__':
-    myParu=Paru("../weights/Athena.pt","../robo.yaml")
-    myParu.detect_image("./381.jpg")
+    myParu=Paru("../weights/Apoll.pt","../robo.yaml")
+    myParu.detect_image("./test.jpg")
     pass
