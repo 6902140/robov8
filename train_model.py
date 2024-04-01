e4b704edb5e9c431243cd6d3881f8b7168b76b43
Zhuiri Xiao <3356142450@qq.com>
2024-04-01 10:35:35 +0800

    add apoll

--- train_model.py
+++ train_model.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 # Load a model
 #load a pre-trained model
-model = YOLO('./weights/yolov8s.pt')  # build from YAML and transfer weights
+model = YOLO('./weights/yolov8n.pt')  # build from YAML and transfer weights
 # Train the model
-model.train(data='./robo.yaml', epochs=300, imgsz=1536,batch=16, workers=8,device="cuda")
+model.train(data='./robo.yaml', epochs=3000, imgsz=1024,batch=32, workers=12,device="cuda",mosaic=0,erasing=0,optimizer="AdamW",momentum=0.9,lr0=0.00125,perspective=0.00012,shear=20,dropout=0.1,degrees=5)
 # model.export(format='')
