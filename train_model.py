cf399f7825046d4e352a3101e12eb755935ca0cd
Zhuiri Xiao <3356142450@qq.com>
2024-04-29 00:34:51 +0800

    新增注意力机制

--- train_model.py
+++ train_model.py
@@ -4,6 +4,6 @@ from ultralytics import YOLO
 # model = YOLO('./weights/yolov8n.pt')  # build from YAML and transfer weights
 # Train the model
 
-model = YOLO('yolov8n-att.yaml')
-model.train(data='./robo.yaml', epochs=3000, imgsz=1536,cls=6.0,dfl=4.0,label_smoothing=0.15,patience=80,batch=32, workers=12,device="cuda",mosaic=0,erasing=0,optimizer="AdamW",momentum=0.9,lr0=0.00125,perspective=0.00012,shear=20,dropout=0.1,degrees=5)
+model = YOLO('yolov8n-att.yaml').load("./weights/Akua-att.pt")
+model.train(data='./robo.yaml', epochs=3000, imgsz=1024,cls=0.65,label_smoothing=0.15,patience=120,batch=32, workers=12,device="cuda",mosaic=0,erasing=0,optimizer="AdamW",momentum=0.9,lr0=0.00125,perspective=0.00012,shear=1,dropout=0.05,degrees=1)
 # model.export(format='')
