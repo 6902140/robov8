ed4ea8b0dd694a546ad3da1baac31b4e04c0b965
Zhuiri Xiao <3356142450@qq.com>
2024-05-01 15:21:25 +0800

    修改训练参数

--- train_model.py
+++ train_model.py
@@ -1,9 +1,8 @@
 from ultralytics import YOLO
 
-#load a pre-trained model
-# model = YOLO('./weights/yolov8n.pt')  # build from YAML and transfer weights
-# Train the model
+# 从预训练模型开始训练
+model = YOLO('./weights/Akua-v0.3.pt')  # build from YAML and transfer weights
 
-model = YOLO('yolov8n-att.yaml').load("./weights/Akua-att.pt")
-model.train(data='./robo.yaml', epochs=3000, imgsz=1024,cls=0.65,label_smoothing=0.03,patience=100,batch=32, workers=12,device="cuda",mosaic=0,erasing=0,optimizer="AdamW",momentum=0.9,lr0=0.00125,perspective=0.00005,shear=1,dropout=0.05,degrees=1)
-# model.export(format='')
+# 从自定义的网络结构开始训练
+# model = YOLO('yolov8n-att.yaml').load("./weights/Akua-att.pt")
+model.train(data='./robo.yaml', epochs=3000, imgsz=1024,cls=0.75,dfl=1.8,cos_lr=True,cache=True,label_smoothing=0.02,patience=100,batch=32, workers=16,device="cuda",mosaic=0,erasing=0,,optimizer="AdamW",momentum=0.9,lr0=0.00125,perspective=0.00005,shear=1,dropout=0.05,degrees=1)
