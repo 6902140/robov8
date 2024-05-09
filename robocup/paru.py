75e6f1024e61ff14ca16b793c317fdd9e33d5645
Zhuiri Xiao <3356142450@qq.com>
2024-05-09 21:14:21 +0800

    add shuffle att onnx v3

--- robocup/paru.py
+++ robocup/paru.py
@@ -48,6 +48,6 @@ class Paru(object):
         return results,detected_imgs
 # just for testing purposes 
 if __name__ == '__main__':
-    myParu=Paru("../weights/Akua-shuffle-att-v2.onnx","../robo.yaml")
-    myParu.detect_image("./test-0.jpg",draw_img=True)
+    myParu=Paru("../weights/Akua-shuffle-att-v3.onnx","../robo.yaml")
+    myParu.detect_image("./test-8.jpg",draw_img=True)
    
