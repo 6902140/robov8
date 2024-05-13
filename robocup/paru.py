ba044f1df7e9cc01be901540cafc369d3860891b
Zhuiri Xiao <3356142450@qq.com>
2024-05-13 22:27:25 +0800

    添加色塊干擾

--- robocup/paru.py
+++ robocup/paru.py
@@ -34,20 +34,21 @@ class Paru(object):
         else:
             image_list=[source]
 
-        results=self.model(image_list,conf=0.2,max_det=15,iou=0.5) # conf 设置置信度下限
+        results=self.model(image_list,conf=0.4,max_det=15,iou=0.5) # conf 设置置信度下限
         result=results[0]
         detected_imgs=[]
 
         detected_imgs.append(result.plot())
         resized_image = cv2.resize(result.plot(), (800, 600))
         if draw_img: # just for show the processed pictures
-            cv2.imshow("test",resized_image)
-            cv2.waitKey(0)
-            print(results)
-            print(results[0].boxes)
+            cv2.imwrite("result.jpg", resized_image)
+            # cv2.imshow("test",resized_image)
+            # cv2.waitKey(0)
+            # print(results)
+            # print(results[0].boxes)
         return results,detected_imgs
 # just for testing purposes 
 if __name__ == '__main__':
-    myParu=Paru("../weights/Akua-p2-pre.pt","../robo.yaml")
-    myParu.detect_image("./test-18.jpg",draw_img=True)
+    myParu=Paru("../weights/Corleone.pt","../robo.yaml")
+    myParu.detect_image("./test-43.jpg",draw_img=True)
    
